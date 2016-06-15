f303f4c906f1db833a349e943ab537863518724c
Joe Walsh <j.thomas.walsh@gmail.com>
2016-06-15 02:58:16 +0000

    simplified data directory structure and modified R script to use environment variables

--- input/IPA_data_incomplete.R
+++ input/IPA_data_incomplete.R
@@ -1,5 +1,6 @@
-
 require(foreign)
-sav_data <- read.spss('data/input/static_box_folder/data/IPA/IPA_data_incomplete.sav')
-write.csv(sav_data, 'data/input/IPA_data_incomplete.csv')
 
+input_file = Sys.getenv("INPUT")
+output_file = Sys.getenv("OUTPUT")
+sav_data = read.spss(input_file)
+write.csv(sav_data, output_file)
