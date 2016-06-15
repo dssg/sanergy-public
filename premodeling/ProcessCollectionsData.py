a4e12cdb8c3ef7b543cc23c912b220cb4c5f706b
bmcinnis <brian.j.mcinnis@gmail.com>
2016-06-15 16:35:10 +0000

    Added function to rename variables

--- premodeling/ProcessCollectionsData.py
+++ premodeling/ProcessCollectionsData.py
@@ -23,14 +23,38 @@ import numpy as np
 # Timeseries data
 from pandas import Series, Panel
 
+# Helper functions
+RULES = [("__c","")]
+def standardize_variable_names(table, RULES):
+	"""
+	Script to standardize the variable names in the tables
+	PARAM DataFrame table: A table returned from pd.read_sql
+	PARAM list[tuples]: A list of tuples with string replacements, i.e., (string, replacement)
+	RET table
+	"""
+	variableNames = list(table.columns.values)
+	standardizedNames = {} # Pandas renames columns with a dictionary object
+	for v in variableNames:
+		f = v
+		for r in RULES:
+			f = f.replace(r[0],r[1])
+		#print '%s to %s' %(v,f)
+		standardizedNames[v] = f
+	table = table.rename(columns=standardizedNames)
+	return table
+
 # Connect to the postgres database
 conn = psycopg2.connect(host=dbconfig.config['host'],
 			user=dbconfig.config['user'],
 			password=dbconfig.config['password'],
 			port=dbconfig.config['port'])
 
+print('connected to postgres')
 # Load the collections data to a pandas dataframe
-collects = pd.read_sql('SELECT * FROM public."Collection_Data__c"',conn,coerce_float=True,params=None)
+collects = pd.read_sql('SELECT * FROM public."Collection_Data__c" limit 10',conn,coerce_float=True,params=None)
+print('data read from postgres')
+
+collects = standardize_variable_names(collects, RULES)
 print(list(collects.columns.values))
 conn.close()
 
