ac35968b92fe8b8b1e395d13f9b132f9a22e5761
IvanaPetrovic <ivana_petrovic@brown.edu>
2016-06-16 23:49:41 +0000

    drake file for IPAsurvey

--- input/IPA_data_incomplete.R
+++ input/IPA_data_incomplete.R
@@ -1,6 +1,6 @@
 require(foreign)
 
-input_file = Sys.getenv("INPUT")
+input_file = Sys.getenv("INPUT1")
 output_file = Sys.getenv("OUTPUT")
 sav_data = read.spss(input_file)
 write.csv(sav_data, output_file)
