e60c021c29612722797e0d82de0989156fe24d06
bmcinnis <brian.j.mcinnis@gmail.com>
2016-06-17 14:45:11 +0000

    Fixes #11 Updated constants

--- premodeling/movePublicToInput.py
+++ premodeling/movePublicToInput.py
@@ -11,8 +11,8 @@ import psycopg2
 import pprint, re
 
 ## Constants
-MOVE_TO = "input"
-MOVE_FROM = "public"
+MOVE_TO = u"input"
+MOVE_FROM = u"public"
 KNOWN_TABLES = [u'Collection_Data__c',
 		u'_IPA_tbl_system_user',
 		u'_IPA_tbl_toilet',
@@ -28,7 +28,7 @@ engine = create_engine('postgresql+psycopg2://%s:%s@%s:%s' %(dbconfig.config['us
 							dbconfig.config['port']))
 connection = engine.connect()
 result = connection.execute("select table_schema, table_name from information_schema.tables;")
-result = [res for res in result.fetchall() if ((res[0]==u'public')&(res[1] in KNOWN_TABLES))]
+result = [res for res in result.fetchall() if ((res[0]==MOVE_FROM)&(res[1] in KNOWN_TABLES))]
 
 for kt in result:
 	print('ALTER TABLE %s."%s" SET SCHEMA %s' %(MOVE_FROM, kt[1], MOVE_TO))
