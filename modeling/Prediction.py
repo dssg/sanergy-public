21edd8dd7acb65f34c11b57c9b5592a825770251
vlachy <jan.vlachy@gmail.com>
2016-06-24 19:07:24 +0000

    Added a predraft of the classes useful for the evaluation.

---
+++ modeling/Prediction.py
@@ -0,0 +1,13 @@
+class Prediction(object):
+"""
+An encapsulating class for toilet waste predictions, so we don't have to worry about what to pass.
+"""
+    def __init__(self):
+        pass
+        #self.lyrics = lyrics
+
+    def getToiletEstimate(self, toilet, day):
+        """
+        Return an estimate of the toilet waste for the given toilet on the given day
+        """
+        pass
