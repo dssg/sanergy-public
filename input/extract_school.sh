38b97ae10955bea9f53f423d9bebf8efcd78a6eb
bmcinnis <brian.j.mcinnis@gmail.com>
2016-06-27 12:33:00 +0000

    Cleaning up comments

--- input/extract_school.sh
+++ input/extract_school.sh
@@ -5,15 +5,13 @@ sheetFirst="SF_check"
 #"Bad" columns
 #Figure them out using csvcut -n ...
 #COLS="$(printf "%s" {23..34}, 35)"
-#COLSTRUCK="$(printf "%s" {25..35}, 36)"
-#COLSTUK="$(printf "%s" {23..34}, 35)"
 
 mkdir tmp
 #Loop over routes and
 in2csv --sheet "$sheetFirst" $INPUT1 | tail -n +2 > tmp/school_by_route.csv
 
 csvcut -n tmp/school_by_route.csv
-#csvcut -C "$COLS" tmp/wheelcart.csv > $OUTPUT0 #tmp/wheelCut.csv
+#csvcut -C "$COLS" tmp/school_by_route.csv > $OUTPUT0 #tmp/school_by_route.csv
 
 rm -r data/input/tmp/
 
