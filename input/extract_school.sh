45d5b10f121327bf8c096a6e1159ff3a056b879c
bmcinnis <brian.j.mcinnis@gmail.com>
2016-06-27 13:19:17 +0000

    Commented out table schema code

--- input/extract_school.sh
+++ input/extract_school.sh
@@ -2,15 +2,17 @@
 
 sheetFirst="SF_check"
 
-#"Bad" columns
 #Figure them out using csvcut -n ...
 #COLS="$(printf "%s" {23..34}, 35)"
 
 mkdir tmp
-#Loop over routes and
+#Read the xlsx file to a csv:
 in2csv --sheet "$sheetFirst" $INPUT1 | tail -n +2 > tmp/school_by_route.csv
 
-csvcut -n tmp/school_by_route.csv
+#Check the file and prepare the postgresql schema
+#in2csv --sheet "$sheetFirst" /mnt/data/sanergy/input/Logistics_schedule.xlsx | tail -n +1 > tmp/school_by_route.csv
+#tail -n +1 tmp/school_by_route.csv | iconv -t ascii | tr [:upper:] [:lower:]| tr ' ' '_' |tr '-' '_' | csvsql -i postgresql 
+
 #csvcut -C "$COLS" tmp/school_by_route.csv > $OUTPUT0 #tmp/school_by_route.csv
 
 rm -r data/input/tmp/
