e90240c8ee106c8b4f3469613b5d446b463e81a4
Ivana Petrovic <ivana_petrovic@brown.edu>
2016-07-01 15:54:57 -0500

    Update run.py

--- run.py
+++ run.py
@@ -7,5 +7,22 @@
 def main(config_file_name):
   #Set up the Logger
   
+  logging.basicConfig(format="%(asctime)s %(message)s",
+                 filename="default.log", level=logging.INFO)
+    log = logging.getLogger("Police EIS")
+
+    screenlog = logging.StreamHandler(sys.stdout)
+    screenlog.setLevel(logging.DEBUG)
+    formatter = logging.Formatter("%(asctime)s - %(name)s: %(message)s")
+    screenlog.setFormatter(formatter)
+    log.addHandler(screenlog)
+      #Load the yaml file.
+    try:
+        with open(config_file_name, 'r') as f:
+            config = yaml.load(f)
+        log.info("Loaded experiment file")
+    except:
+        log.exception("Failed to get experiment configuration file!")
   
-  #Load the yaml file.
+  
+
