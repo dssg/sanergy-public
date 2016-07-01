a9113573266698cc599998506c95c349ae48ad92
vlachy <jan.vlachy@gmail.com>
2016-07-01 14:16:37 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,1 +1,11 @@
 #The first draft of the enveloping script that runs the pipeline.
+
+#Import external modules
+
+#Import the internal modules
+
+def main(config_file_name):
+  #Set up the Logger
+  
+  
+  #Load the yaml file.
