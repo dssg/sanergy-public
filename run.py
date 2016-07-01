0478463984bc5db057ba43fa052e6301a7d319a3
Ivana Petrovic <ivana_petrovic@brown.edu>
2016-07-01 16:03:25 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,12 +1,15 @@
 #The first draft of the enveloping script that runs the pipeline.
-
+import logging
+import yaml
 #Import external modules
+
+import . from datasets
 
 #Import the internal modules
 
 def main(config_file_name):
   #Set up the Logger
-  
+  # define logging
   logging.basicConfig(format="%(asctime)s %(message)s",
                  filename="default.log", level=logging.INFO)
     log = logging.getLogger("Police EIS")
@@ -23,6 +26,11 @@ def main(config_file_name):
         log.info("Loaded experiment file")
     except:
         log.exception("Failed to get experiment configuration file!")
+  # Brian will work on the following 
+  grab_from_dataset(config) #this creates df features and labels in the postgres
+  [train,test]=splits(features, labels) # this 
+  
+  
   
   
 
