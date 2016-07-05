d42986d76b6c868bc3ac2b5adcb1da71e837df4a
Brian McInnis <brian.j.mcinnis@gmail.com>
2016-07-05 11:40:15 -0500

    Updating to loop through each experiment

--- run.py
+++ run.py
@@ -26,15 +26,36 @@ def main(config_file_name):
         log.info("Loaded experiment file")
     except:
         log.exception("Failed to get experiment configuration file!")
-  # Brian will work on the following
-  # 1. Create the labels / features data set in Postgres
-  grab_from_dataset(config) #this creates df features and labels in the postgres
-  # 2. The function splits should take in the config file, so that we can train every day / seven days / month, etc.
-  [folds]=splits(config) # this will be a list of date ranges for train and test. Let's imagine that train and test are sets of pairs (start/end date), we pass that list of tuples of models.py and train each of the models on the list tuples.
-"""
-    [{"train":(start, end),
-      "test":(start, end)}, ... Fold 2 ...]  
-"""
+
+  # 1. Generate all experiments [Jan]
+  experiments = generate_experiments(config)
+  """
+      loop through the config variables to generate a list of experiments to run
+      [{model: "randomForest",
+        parameters: {},
+        situation_vars: {prediction_window: (then, now)}...}, ...experiment 2... ]
+  """
+
+  # Loop through each of the experiments
+  for experiment in experiments:
+  
+    # Brian will work on the following
+    # 2. Create the labels / features data set in Postgres
+    grab_from_dataset(config) #this creates df features and labels in the postgres
+    
+    # 3. The function splits should take in the config file, so that we can train every day / seven days / month, etc.
+    [folds]=splits(config) # this will be a list of date ranges for train and test. Let's imagine that train and test are sets of pairs (start/end date), we pass that list of tuples of models.py and train each of the models on the list tuples.
+    """
+        [{"train":(start, end),
+          "test":(start, end)}, ... Fold 2 ...]  
+    """
+    # 4. Folds are passed to models functions
+    run_models_on_folds([folds], config):
+      storing_results_from_modeling = [{}]
+      for fold in folds
+        DF{labels: train, features: train}, DF{labels: test, features: test} = grab_from_features_and_labels(fold)
+      
+      
   
   
   
