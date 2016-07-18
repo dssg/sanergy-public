0686dc3f40d34f39a421a4a33e9cfe06a17fd211
vlachy <jan.vlachy@gmail.com>
2016-07-18 15:01:08 +0000

    Added the test for Experiment.py

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -8,19 +8,21 @@ class ExperimentTest(unittest.TestCase):
         with open("tests/test.yaml", 'r') as f:
             self.config = yaml.load(f)
 
-    def test_true(self):
-        self.assertEqual(True, False)
-        self.assertEqual(1, 2)
-    #def test_Yaml(self):
-    #    self.assertEqual(self.config['model'][1],"RandomForest")
-    #    self.assertIsInstance(self.config['db'],'dict')
-    #    self.assertIsInstance(self.config['parameters'],'dict')
-    #    self.assertIsInstance(self.config['parameters']['RandomForest'],'list')
+    #def test_true(self):
+    #    self.assertEqual(True, False)
+    #    self.assertEqual(1, 2)
+    def test_Yaml(self):
+        self.assertEqual(self.config['model'][0],"RandomForest")
+        #print(type(self.config['db']))
+        self.assertIsInstance(self.config['db'],dict)
+        self.assertIsInstance(self.config['parameters'],dict)
+        self.assertIsInstance(self.config['parameters']['RandomForest']['n_estimators'],list)
 
     def test_experiments(self):
          experiments = generate_experiments(self.config)
-         self.assertEqual(len(experiments[1]),9)
-         self.assertIsInstance(experiments[23].parameters,'dict')
+         #print(experiments[1].parameters)
+         self.assertEqual(len(experiments[1].parameters),9)
+         self.assertIsInstance(experiments[23].parameters,dict)
 
 if __name__ == '__main__':
     unittest.main()
