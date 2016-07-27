de553b7effa4e805924d06ba87024fab9a5582eb
vlachy <jan.vlachy@gmail.com>
2016-07-27 16:25:56 +0000

    Finished and tested the basic staffing model

--- modeling/Staffing.py
+++ modeling/Staffing.py
@@ -4,6 +4,7 @@ class Staffing(object):
     N_DAYS = 7
     COL_ROUTE = 'Area'
     COL_DAYS = [str(d) for d in range(0,N_DAYS)]
+    COLLECT = 1
     """
     Provide a workforce schedule for a given collection schedule.
 
@@ -14,18 +15,18 @@ class Staffing(object):
         """
 
         Args:
-          schedule (dataframe): For each toilet and day, collect vs not collection, and which area/route it is
-          waste (dataframe): For each toilet and day, the predicted amount of waste accumulated
+          schedule (dataframe): For each toilet, a column for a day, collect vs not collection, and which area/route it is
+          waste (dataframe): For each toilet, a column for a day, the predicted amount of waste accumulated.
           parameters (dict):
             W: weight limit per worker per day
             D: day limit of workdays per week per worker
             N: number of available workers
-            NR: minimum worker per route (assume 2, but in fact some need 3 -> fix later)
+            NR: minimum number of workers required per route (assume 2, but in fact some need 3 -> fix later)
         """
         self.schedule = schedule
         self.waste = waste
         self.parameters = staffing_parameters
-        self.config
+        self.config = config
 
 
     def preprocess(self):
@@ -34,8 +35,10 @@ class Staffing(object):
         T: number of toilets (from schedule)
         routes: ... (from schedule)
         """
-        self.routes = self.schedule[COL_ROUTE].unique()
-        self.route_waste = {(d,r) : self.waste.groupby(['Area',d])[config['cols']['feces']].sum() for d in COL_DAYS for r in self.routes} #TODO
+        self.routes = self.schedule[self.COL_ROUTE].unique()
+        #Only include the routes that have at least one toilet to collect
+        self.is_the_route_collected_on_day = {(d,r) : self.schedule.loc[self.schedule[self.COL_ROUTE]==r,d].sum() > 0 for d in self.COL_DAYS for r in self.routes}
+        self.route_waste = {(d,r) : self.waste.loc[((self.schedule[self.COL_ROUTE] == r) & (self.schedule[d]==self.COLLECT)),d].sum() for d in self.COL_DAYS for r in self.routes}
 
     def staff(self):
         """
@@ -43,7 +46,7 @@ class Staffing(object):
         Minimize workers needed, given the constraints
         Note that the problem can be essentially decompose by area as we assume that workers don't cross areas in one day (and possibly the entire week?).
 
-        Step 1:
+        Step 1 [DONE]:
           * Routes given, just assign the number of workers depending on how much waste we expect?
           * In fact, one could consider each area to be a route and it will give a number of people assigned to an area, assuming they can then determine and divide the routes among themselves.
           * Alternatively, could determine the routes by just clustering toilets, putting together toilets so that we just about fill up Worker's carry limit per route
@@ -86,34 +89,35 @@ class Staffing(object):
         assign_vars = {}
         for c in range(0,self.parameters['N']):
             for i_r, r in enumerate(self.routes):
-                for d in range(0, N_DAYS):
+                for d in self.COL_DAYS:
                     v_name = 'z' + str(c) + str(i_r) + str(d)
                     #This also handles the objective function...
-                    assign_vars[c,i_r,d] = s.addVar(v_name, vtype='B', obj=1.0)
+                    assign_vars[c,r,d] = s.addVar(v_name, vtype='B', obj=1.0)
+
 
         #Constraints
         for c in range(0,self.parameters['N']):
             #1) Collector workday limits
             w_name = 'Worker_' + str(c)
-            coeffs_worker = {assign_vars[(c,i_r,d)] : 1 for d in range(0,N_DAYS) for i_r,r in enumerate(self.routes)}
-            s.addCons(coeffs = coeffs_worker , rhs=self.parameters['D'] name=w_name)
-        for d in range(0,N_DAYS):
+            coeffs_worker = {assign_vars[(c,r,d)] : 1 for d in self.COL_DAYS for i_r,r in enumerate(self.routes)}
+            s.addCons(coeffs = coeffs_worker , rhs=self.parameters['D'], name=w_name)
+        for d in self.COL_DAYS:
             for i_r,r in enumerate(self.routes):
-                 #2) Collector weight limits on routes. For each route: assign 2+ workers and also more than the predicted weight per the route
-                 #3) Assign n+ workers on the route
-                 route_weight_name = 'Route_Weight_' + str(i_r) + "_" + str(d)
-                 route_collector_minimum_name = 'Route_Minimum_' + str(i_r) + "_" + str(d)
-                 coeffs_weight = {assign_vars[(c,i_r,d)] : 1 for c in range(0,self.parameters['N'])} #sum_c z_crd >= w_rd / W
-                 coeffs_collectors = {assign_vars[(c,i_r,d)] : 1 for c in range(0,self.parameters['N'])}
-                 s.addCons(coeffs = coeffs_weight, lhs = self.route_waste[d, r] / self.parameters['W'], rhs=None, name= route_weight_name)
-                 s.addCons(coeffs = coeffs_collectors, lhs = self.parameters['WR'], rhs=None, name= route_collector_minimum_name)
+                #2) Collector weight limits on routes. For each route: assign 2+ workers and also more than the predicted weight per the route
+                #3) Assign n+ workers on the route
+                route_weight_name = 'Route_Weight_' + str(i_r) + "_" + str(d)
+                route_collector_minimum_name = 'Route_Minimum_' + str(i_r) + "_" + str(d)
+                coeffs_weight = {assign_vars[(c,r,d)] : 1 for c in range(0,self.parameters['N'])} #sum_c z_crd >= w_rd / W
+                coeffs_collectors = {assign_vars[(c,r,d)] : 1 for c in range(0,self.parameters['N'])}
+
+                weight_limit =  self.is_the_route_collected_on_day[d,r]  * self.route_waste[d, r] / self.parameters['W']
+
+                s.addCons(coeffs = coeffs_weight, lhs =weight_limit, rhs=None, name= route_weight_name)
+                s.addCons(coeffs = coeffs_collectors, lhs = collect_limit, rhs=None, name= route_collector_minimum_name)
 
         #Objective function
         #Done: See in Vars
-
         s.optimize()
 
-        s.printStatistics()
-        # retrieving the best solution
-        solution = s.getBestSol()
-        return(s)
+        #.printStatistics()
+        return(s, assign_vars)
