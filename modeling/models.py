751df970cf8056ae6700a6b2e5470307f973891e
IvanaPetrovic <ivana_petrovic@brown.edu>
2016-08-16 16:30:40 +0000

    add static models

--- modeling/models.py
+++ modeling/models.py
@@ -239,6 +239,15 @@ class WasteModel(object):
             C=self.parameters['C'],
             epsilon=self.parameters['epsilon'],
             kernel=self.parameters['kernel'])
+        elif self.modeltype == 'StaticModel'
+            return StaticModel (
+                parameters=self.parameters
+                )
+        elif self.modeltype == 'AdvancedStaticModel'
+            return AdvancedStaticModel (
+                parameters=self.parameters
+                )
+
         # elif self.modeltype == 'SGDRegressor' :
         #     print(self.parameters)
         #     return linear_model.SGDRegressor(
