d6f98126cb53736f8e9fb887200a4b3592348c55
Matt Bauman <mbauman@uchicago.edu>
2016-09-02 19:58:15 +0000

    toiletdensity sham table depends upon toiletdistances

--- input/geo_spatial_toilets_sham.sql
+++ input/geo_spatial_toilets_sham.sql
@@ -11,7 +11,7 @@ select "Toilet__c" as "ToiletID",
 		   	into premodeling.toilethistory
 			from input."Collection_Data__c"
 			group by "ToiletID";
--- group by each toilet and produce the ToiletID, and min/max 
+-- group by each toilet and produce the ToiletID, and min/max
 --  for each collection date range
 
 /* Create a sham toiletdensity table. There's a circular dependency between
@@ -23,6 +23,33 @@ select "Toilet__c" as "ToiletID",
  * TODO: VERIFY THAT USING THIS SHAM TABLE DOESN'T MESS UP THE GENERATION OF
  *       THE REAL TABLE
  */
+DROP TABLE IF EXISTS premodeling.toiletdensity;
+-- similar drop if exists
+select "ToiletID",
+		ST_Transform(ST_SetSRID(ST_MakePoint(min("Latitude"), min("Longitude")), 4326),21037) as "Point"
+-- #4326 is WGS84, but that one is not linear but spherical -> difficult to compute distances
+-- #21037 is one of the 4 systems in Kenya and contains Nairobi
+	   	into premodeling.toiletdensity
+			from input."Collection_Data__c" join input."tblToilet"
+			on "Toilet__c"="ToiletID"
+		group by "ToiletID";
+
+-- Third, save the sub-sub query to the toilet distances table
+DROP TABLE IF EXISTS premodeling.toiletdistances;
+select sub.* into premodeling.toiletdistances from
+-- Second, create a sub query of the toilet density table, calculating
+--  the distances between each toilet
+	(select z."ToiletID",
+			s."ToiletID"  as "NeighborToiletID",
+			ST_Distance(z."Point", s."Point") as "Distance",
+			CAST(ST_DWithin(z."Point", s."Point", 5.0) as INT) as "5m",
+			CAST(ST_DWithin(z."Point", s."Point", 25.0) as INT) as "25m",
+			CAST(ST_DWithin(z."Point", s."Point", 50.0) as INT) as "50m",
+			CAST(ST_DWithin(z."Point", s."Point", 100.0) as INT) as "100m" from premodeling.toiletdensity z,
+--  First, merge the toilet density table onto itself, where ID != ID
+		(select * from premodeling.toiletdensity) s
+			where (s."ToiletID" != z."ToiletID")) sub;
+
 DROP TABLE IF EXISTS premodeling.toiletdensity;
 
 -- Create the sham toilet density
